9f3a428ba210a903648bf019806249ff87a0166d
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-01-20 02:36:19 +0900

    게임 서버 개발2

--- Assets/Scripts/PhotonPUN/NetworkManager.cs
+++ Assets/Scripts/PhotonPUN/NetworkManager.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
+using System.Diagnostics;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
+    [Header("Debug Option")]
+    [Tooltip("Debug Massage On/Off Checkbox")]
+    public bool debug = true;
+    public GameObject severConnectBtn;
+
     private void Awake()
     {
         PhotonNetwork.ConnectUsingSettings();
@@ -13,6 +20,30 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     public override void OnConnectedToMaster()
     {
-        print("Connected");
+        if(debug)
+            print("Connected Sever");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        if (debug)
+            print("Join The Lobby");
+
+        severConnectBtn.SetActive(true);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinOrCreateRoom("MyRoom", new RoomOptions { MaxPlayers = 4 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (debug)
+            print("Room In");
+
+        PhotonNetwork.LoadLevel("LevelOne");
     }
 }
