1b4629268550744e97187e684fb713a95e057a20
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-01-20 01:46:02 +0900

    게임 서버 개발1

---
+++ Assets/Scripts/PhotonPUN/NetworkManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Connected");
+    }
+}
